11f2c1d6c5f3653ec970e37376eff49ffe5890f5
Your Name <you@example.com>
2019-08-08 21:01:33 -0300

    faltaban comas campeona

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-fetch('https://reqres.in/api/user')
-    .then(response => response.json())
-    .then(res => {
-        // console.log(res.data);
-        const {data} = res
-        data.map(({id, name}) => console.log(`id: ${id}\nname: ${name}`))
-    })
+fetch('https://raw.githubusercontent.com/Marimariana/fetch/master/cv.json')
+.then (res => res.json())
+.then (res => console.log(res))
